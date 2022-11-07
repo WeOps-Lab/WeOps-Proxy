73886b7de7eca5fbb222c354684cfb14294af791
DESKTOP-6A77J02\Administrator <15875339926@139.com>
2022-11-07 18:24:08 +0800

    Makefile 新增 release

--- python-sdk/setup.py
+++ python-sdk/setup.py
@@ -5,7 +5,7 @@ setup(
     version="1.0",
     packages=find_packages(),
     install_requires=[
-        "PyYaml",
-        "python-consul"
+        "PyYaml==6.0",
+        "python-consul==1.1.0"
     ]
 )
