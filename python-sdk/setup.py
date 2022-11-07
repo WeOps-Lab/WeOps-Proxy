810e067685d87b585d429e79e3d86ba9d74ce537
DESKTOP-6A77J02\Administrator <15875339926@139.com>
2022-11-07 18:21:40 +0800

    setup.py的install_requires 不需要依赖weopsproxy

--- python-sdk/setup.py
+++ python-sdk/setup.py
@@ -4,9 +4,8 @@ setup(
     name="WeOpsProxy",
     version="1.0",
     packages=find_packages(),
-    install_requires = [
+    install_requires=[
         "PyYaml",
-        "python-consul",
-        "weopsproxy"
-        ]
+        "python-consul"
+    ]
 )
