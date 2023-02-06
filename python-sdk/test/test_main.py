1d12c9b896b29bab4e517fbb9f0851188fa55fec
ericchen <ericchen@canway.net>
2023-02-06 12:42:38 +0800

    update

--- python-sdk/test/test_main.py
+++ python-sdk/test/test_main.py
@@ -5,14 +5,14 @@ from test_env import *
 class TestClass:
     def test_初始化client(self):
         WeOpsProxyClient(
-            consul_host=consul_ip,
-            consul_port=consul_port
+            consul_host=CONSUL_IP,
+            consul_port=CONSUL_PORT
         )
 
     def test_生成conul的key(self):
         client = WeOpsProxyClient(
-            consul_host=consul_ip,
-            consul_port=consul_port
+            consul_host=CONSUL_IP,
+            consul_port=CONSUL_PORT
         )
         keys = ["cisco_cw", "h3c_cw", "huawei_cw"]
         for a in keys:
@@ -24,8 +24,8 @@ class TestClass:
 
     def test_生成snmp采集任务的values(self):
         client = WeOpsProxyClient(
-            consul_host=consul_ip,
-            consul_port=consul_port
+            consul_host=CONSUL_IP,
+            consul_port=CONSUL_PORT
         )
         client._get_snmp_task(task_id="cisco_cw", task_address="192.168.165.200", task_module="cisco_cw", interval="60s",timeout="60s",auth={
             "username": "cisco123",
@@ -39,15 +39,15 @@ class TestClass:
     
     def test_生产ipmi采集任务的vaules(self):
       client = WeOpsProxyClient(
-            consul_host=consul_ip,
-            consul_port=consul_port
+            consul_host=CONSUL_IP,
+            consul_port=CONSUL_PORT
         )
       client._get_ipmi_task(task_id="ipmi_task_1",task_address="10.10.10.10",userid="USERID",password="password",interval="60s",timeout="60s")
 
     def test_添加snmp_v2任务(self):
         client = WeOpsProxyClient(
-            consul_host=consul_ip,
-            consul_port=consul_port
+            consul_host=CONSUL_IP,
+            consul_port=CONSUL_PORT
         )
         client.put_snmp_v2_task(zone="default",
                                 task_id="cisco_v2",
@@ -57,8 +57,8 @@ class TestClass:
 
     def test_添加snmp_v3任务(self):
         client = WeOpsProxyClient(
-            consul_host=consul_ip,
-            consul_port=consul_port
+            consul_host=CONSUL_IP,
+            consul_port=CONSUL_PORT
         )
         client.put_snmp_v3_task(zone="default", task_id="cisco_v3", task_address="192.168.165.200", task_config="cisco_cw",
                                 username="cisco123",
@@ -71,87 +71,87 @@ class TestClass:
 
     def test_查询snmp任务(self):
         client = WeOpsProxyClient(
-            consul_host=consul_ip,
-            consul_port=consul_port
+            consul_host=CONSUL_IP,
+            consul_port=CONSUL_PORT
         )
         client.get_snmp_task(zone="default", task_id="cisco_v2")
         client.get_snmp_task(zone="default", task_id="cisco_v3")
 
     def test_创建全局配置(self):
         client = WeOpsProxyClient(
-            consul_host=consul_ip,
-            consul_port=consul_port
+            consul_host=CONSUL_IP,
+            consul_port=CONSUL_PORT
         )
         client.put_global_config(
-            module="snmp", config_id="cisco_cw", config=cisco_snmp_config)
+            module="snmp", config_id="cisco_cw", config=CISCO_SNMP_CONFIG)
 
     def test_查询全局配置(self):
         client = WeOpsProxyClient(
-            consul_host=consul_ip,
-            consul_port=consul_port
+            consul_host=CONSUL_IP,
+            consul_port=CONSUL_PORT
         )
         client.get_global_config(module="snmp", config_id="cisco_cw")
 
     def test_删除全局配置(self):
         client = WeOpsProxyClient(
-            consul_host=consul_ip,
-            consul_port=consul_port
+            consul_host=CONSUL_IP,
+            consul_port=CONSUL_PORT
         )
         client.delete_global_config(module="snmp", config_id="h3c")
 
     def test_获取接入点(self):
         client = WeOpsProxyClient(
-            consul_host=consul_ip,
-            consul_port=consul_port
+            consul_host=CONSUL_IP,
+            consul_port=CONSUL_PORT
         )
         print(client.get_access_points())
 
     def test_写入指标(self):
         client = WeOpsProxyClient(
-            consul_host=consul_ip,
-            consul_port=consul_port
+            consul_host=CONSUL_IP,
+            consul_port=CONSUL_PORT
         )
         client.put_metric(metric_record="cw_export_up",metric_expr="""irate(cw_CiscoSwitch_ifInOctets{module="cisco_switch"}[1m])""")
     
     def test_获取指标(self):
         client = WeOpsProxyClient(
-            consul_host=consul_ip,
-            consul_port=consul_port
+            consul_host=CONSUL_IP,
+            consul_port=CONSUL_PORT
         )
         print(client.get_metric(metric_record="cw_export_up"))
 
     def test_删除指标(self):
         client = WeOpsProxyClient(
-            consul_host=consul_ip,
-            consul_port=consul_port
+            consul_host=CONSUL_IP,
+            consul_port=CONSUL_PORT
         )
         client.delete_metric(metric_record="cw_export_up")
 
     def test_创建IPMI监控任务(self):
       client = WeOpsProxyClient(
-            consul_host=consul_ip,
-            consul_port=consul_port
+            consul_host=CONSUL_IP,
+            consul_port=CONSUL_PORT
         )
-      client.put_ipmi_task(task_id="test_ipmi_1",task_address=ipmi_ip,userid=ipmi_user,password=ipmi_password,zone="default")
+      client.put_ipmi_task(task_id="test_ipmi_1",task_address=IPMI_IP,userid=IPMI_USER,password=IPMI_PASSWORD,zone="default")
     
     def test_获取IPMI监控任务(self):
       client = WeOpsProxyClient(
-        consul_host=consul_ip,
-        consul_port=consul_port
+        consul_host=CONSUL_IP,
+        consul_port=CONSUL_PORT
       )
       client.get_ipmi_task(zone="default",task_id="test_ipmi_1")
     
     def test_删除IPMI监控任务(self):
       client = WeOpsProxyClient(
-        consul_host=consul_ip,
-        consul_port=consul_port
+        consul_host=CONSUL_IP,
+        consul_port=CONSUL_PORT
       )
       client.delete_ipmi_task(zone="default",task_id="test_ipmi_1")
     
     def test_删除snmp任务(self):
         client = WeOpsProxyClient(
-            consul_host=consul_ip,
-            consul_port=consul_port
+            consul_host=CONSUL_IP,
+            consul_port=CONSUL_PORT
         )
         client.delete_snmp_task(zone="default", task_id="cisco_v2")
         client.delete_snmp_task(zone="default", task_id="cisco_v3")
